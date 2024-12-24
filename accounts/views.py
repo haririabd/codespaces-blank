bdeb1b387227504ebad12319fe0a97b050f18dc4
Hariri A. <110659046+haririabd@users.noreply.github.com>
2024-12-24 04:39:06 +0000

    creating forms

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
-def login(request):
-    return render(request, 'accounts/login.html', {})
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("/")
+        else:
+            messages.info(request, 'Username OR password is incorrect')
+    context = {}
+    return render(request, 'accounts/login.html', context)
 
 def register(request):
     return render(request, 'accounts/register.html', {})
