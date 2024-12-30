42d53abc01b235d470bafb99d1ffc50806f7992f
Hariri A. <110659046+haririabd@users.noreply.github.com>
2024-12-30 14:29:41 +0000

    testing simple db creation

--- dblearning/admin.py
+++ dblearning/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import State, Category, Store, Brand
 # Register your models here.
+
+admin.site.register(State)
+admin.site.register(Category)
+admin.site.register(Brand)
+
+
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ("name", "code", "state")
+
+admin.site.register(Store, StoreAdmin)
--- dblearning/models.py
+++ dblearning/models.py
@@ -1,6 +1,31 @@
 from django.db import models
 
 # Create your models here.
-class Person(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+class Brand(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class State(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=30)
+
+    # It’s suggested, but not required, that the name of a ForeignKey field (state in the example below) be the name of the model (State in below example), lowercase.
+    # You can call the field whatever you want
+    state = models.ForeignKey(State, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
