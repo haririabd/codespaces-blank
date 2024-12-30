8b8e2ce9572758ec1b1313fae3f64840c6019042
Hariri A. <110659046+haririabd@users.noreply.github.com>
2024-12-30 19:35:59 +0000

    DB query working

--- dblearning/models.py
+++ dblearning/models.py
@@ -4,12 +4,6 @@ from django.contrib.auth.models import User
 # Create your models here.
 # Profile referred to geeks for geek: https://www.geeksforgeeks.org/how-to-extend-user-model-in-django/
 # Also here : https://medium.com/@karimdhrif4444/mastering-user-management-comprehensive-guide-to-extending-djangos-user-model-51c2ccd793d4
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    website = models.URLField(blank=True)
-
-    def __str__(self):
-        return self.user.username
 
 class Brand(models.Model):
     name = models.CharField(max_length=50)
@@ -39,3 +33,11 @@ class Store(models.Model):
 
     def __str__(self):
         return self.name
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    website = models.URLField(blank=True)
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
--- dblearning/views.py
+++ dblearning/views.py
@@ -7,4 +7,6 @@ def index(request):
 
 def profile_view(request, username):
     profile = Profile.objects.get(user__username=username)
+    current_user = Profile.objects.get(pk=request.user.id).store.state.name
+    print(current_user)
     return render(request, 'dblearning/profile.html', {'profile': profile})
